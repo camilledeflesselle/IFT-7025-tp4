19f256d8298f3b7fd85a84b11f7155f4e7a7542a
deflessc <camille.deflesselle@edu.esiee.fr>
2022-04-08 15:54:55 -0400

    fichier qui charge les datasets OK

--- Code/load_datasets.py
+++ Code/load_datasets.py
@@ -39,7 +39,8 @@ def load_iris_dataset(train_ratio):
     f.close()
     lines = [line.split(",") for line in lines if line]
     data=np.array([line[:4] for line in lines], dtype=float)
-    labels=np.array([conversion_labels[line[-1]] for line in lines], dtype=float)
+    # l'étiquette à prédire est la dernière colonne
+    labels=np.array([conversion_labels[line[-1]] for line in lines], dtype=int)
     nrow = len(data)
     indices = np.arange(nrow)
 
@@ -55,7 +56,6 @@ def load_iris_dataset(train_ratio):
 
     # la fonction retourne 4 matrices de type Numpy. 
     return (train, train_labels, test, test_labels)
-	
 	
 	
 def load_wine_dataset(train_ratio):
@@ -85,12 +85,22 @@ def load_wine_dataset(train_ratio):
 
     # Le fichier du dataset est dans le dossier datasets en attaché 
     f = open('datasets/binary-winequality-white.csv', 'r')
+    lines = [line.strip() for line in f.readlines()]
+    f.close()
+    lines = [line.split(",") for line in lines if line]
+    data=np.array([line[:-1] for line in lines], dtype=float)
+    # l'étiquette à prédire est la dernière colonne
+    labels=np.array([line[-1] for line in lines], dtype=int)
+    nrow = len(data)
+    indices = np.arange(nrow)
+    np.random.shuffle(indices)
+    split = math.floor(train_ratio * nrow)
 
+    train_idx,    test_idx    = indices[:split],   indices[split:]
+    train,        test        = data[train_idx],   data[test_idx]
+    train_labels, test_labels = labels[train_idx], labels[test_idx]
 	
-    # TODO : le code ici pour lire le dataset
-    
-	
-	# La fonction doit retourner 4 structures de données de type Numpy.
+	# La fonction retourne 4 structures de données de type Numpy.
     return (train, train_labels, test, test_labels)
 
 def load_abalone_dataset(train_ratio):
@@ -99,7 +109,7 @@ def load_abalone_dataset(train_ratio):
 
     Args:
         train_ratio: le ratio des exemples (ou instances) qui vont servir pour l'entrainement,
-        le rest des exemples va etre utilisé pour les test.
+        le reste des exemples va etre utilisé pour les test.
 
     Retours:
         Cette fonction doit retourner 4 matrices de type Numpy: train, train_labels, test, et test_labels
@@ -116,7 +126,28 @@ def load_abalone_dataset(train_ratio):
         - test_labels : contient les étiquettes pour chaque exemple dans test, de telle sorte
           que : test_labels[i] est l'etiquette pour l'exemple test[i]
     """
-    f = open('datasets/abalone-intervalles.csv', 'r') # La fonction doit retourner 4 matrices (ou vecteurs) de type Numpy.
-    return (train, train_labels, test, test_labels)
+    random.seed(1) # Pour avoir les meme nombres aléatoires à chaque initialisation.
+    f = open('datasets/abalone-intervalles.csv', 'r') 
+    lines = [line.strip() for line in f.readlines()]
+    f.close()
 
-print(load_iris_dataset(0.7))
+    lines = [line.split(",") for line in lines if line]
+    conversion_sexe = {'M': 0, 'F' : 1, 'I' : 2}
+    for line in lines : # on convertit les strings en integer
+      line[0] = conversion_sexe[line[0]]
+    data=np.array([line[:-1] for line in lines], dtype=float)
+
+    # l'étiquette à prédire est la dernière colonne
+    labels=np.array([line[-1] for line in lines], dtype=float)
+
+    nrow = len(data)
+    indices = np.arange(nrow)
+    np.random.shuffle(indices)
+    split = math.floor(train_ratio * nrow)
+
+    train_idx,    test_idx    = indices[:split],   indices[split:]
+    train,        test        = data[train_idx],   data[test_idx]
+    train_labels, test_labels = labels[train_idx], labels[test_idx]
+
+    # La fonction retourne 4 matrices (ou vecteurs) de type Numpy.
+    return (train, train_labels, test, test_labels)
