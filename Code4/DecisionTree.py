6033d37bac986340118b186bccc5d98d7543e5f2
deflessc <camille.deflesselle@edu.esiee.fr>
2022-04-27 23:57:25 -0400

    ok pour attribut factoriels

--- Code4/DecisionTree.py
+++ Code4/DecisionTree.py
@@ -9,13 +9,13 @@ se fera en utilisant les méthodes train, predict et evaluate de votre code.
 """
 
 import numpy as np
-
+from metrics import show_metrics
 
 # le nom de votre classe
 # DecisionTree pour l'arbre de décision
 # NeuralNet pour le réseau de neurones
 
-class Classifier: #nom de la class à changer
+class DecisionTree: #nom de la class à changer
 
 	def __init__(self, **kwargs):
 		"""
@@ -23,9 +23,47 @@ class Classifier: #nom de la class à changer
 		Vous pouvez passer d'autre paramètres au besoin,
 		c'est à vous d'utiliser vos propres notations
 		"""
+		self.namesAtt = ["att"]
+		self.typeAtt = ["factor", "factor"]
+
+	def calculateEntropy(self, vector):
+		"""
+		calcule l'entropie d'un vecteur
+		"""
+		_, uniqueClassesNumber = np.unique(vector, return_counts = True)
+		proba = uniqueClassesNumber / uniqueClassesNumber.sum()
+		return sum(-proba * np.log2(proba))
 		
-		
-	def train(self, train, train_labels): #vous pouvez rajouter d'autres attributs au besoin
+	def calculateGlobalEntropie(self, vector, labels):
+		"""
+		calcul le gain d'information pour un attribut
+		"""
+		global_entropie = 0
+		for i in np.unique(vector):
+			index = np.where(vector==i)[0]
+			pData = len(index)/len(vector)
+			dataB = labels[index]  # classes des individus dont la valeur de l'attribut est i
+			global_entropie += pData * self.calculateEntropy(dataB)
+		return global_entropie
+
+	def classifyData(self, vector):
+		uniqueClasses, uniqueClassesCounts = np.unique(vector, return_counts = True)
+		return uniqueClasses[uniqueClassesCounts.argmax()]
+
+	def determineBestColumn(self, train, train_labels):
+		entropie_init = self.calculateEntropy(train_labels)
+		#print(entropie_init)
+		best_column = None
+		max_gain = None
+		for i in range(train.shape[1]) :
+			gain = entropie_init - self.calculateGlobalEntropie(train[:, i], train_labels)
+			#print(gain)
+			if max_gain == None or max_gain < gain :
+				max_gain = gain
+				best_column = i
+		return best_column
+
+	def train(self, train, train_labels, current_depth = 0, max_depth = None): #vous pouvez rajouter d'autres attributs au besoin
 		"""
 		C'est la méthode qui va entrainer votre modèle,
 		train est une matrice de type Numpy et de taille nxm, avec 
@@ -36,28 +74,55 @@ class Classifier: #nom de la class à changer
 		
 		vous pouvez rajouter d'autres arguments, il suffit juste de
 		les expliquer en commentaire
-		
+		construction de l'abre de décision de manière récursive
 		"""
-        
-	def predict(self, x):
+		if max_depth == None :
+			max_depth = train.shape[1] +1
+		if current_depth == max_depth or len(np.unique(train_labels)) == 1: # si l'arbre est assez profond ou si une seule classe on classifie
+			return self.classifyData(train_labels)
+		else :
+			current_depth += 1
+			racine = self.determineBestColumn(train, train_labels)
+			uniquesValuesRacine = np.unique(train[:, racine])
+			decisionSubTree = {}
+			for value in uniquesValuesRacine :
+				question = "Attribut {} == {}".format(racine, value)
+				index = np.where(train[:, racine]==value)[0]
+				dataB = train[index]  # classes des individus dont la valeur de l'attribut est i
+				indexB = train_labels[index]
+				yesAnswer = self.train(dataB, indexB, current_depth, max_depth)
+				decisionSubTree[question] = yesAnswer
+			return decisionSubTree
+    
+	def separate_att(self, question):
+		attribute, value = question.split(" == ")
+		_, attributeIndex = attribute.split(" ")
+		return [int(attributeIndex), float(value)]
+
+	def predict(self, x, decision_tree):
 		"""
 		Prédire la classe d'un exemple x donné en entrée
 		exemple est de taille 1xm
 		"""
+		if not isinstance(decision_tree, dict):
+			return decision_tree
+		questions = list(decision_tree.keys())
+		list_map = np.array([self.separate_att(question) for question in questions])
+		index = np.where(list_map[:, 1] == x[list_map[0, 0]])[0][0]
+		answer = decision_tree[questions[index]]
+		return self.predict(x, answer)
+		
         
-	def evaluate(self, X, y):
+	def evaluate(self, X, y, decision_tree):
 		"""
 		c'est la méthode qui va évaluer votre modèle sur les données X
 		l'argument X est une matrice de type Numpy et de taille nxm, avec 
 		n : le nombre d'exemple de test dans le dataset
 		m : le nombre d'attributs (le nombre de caractéristiques)
-		
 		y : est une matrice numpy de taille nx1
-		
 		vous pouvez rajouter d'autres arguments, il suffit juste de
 		les expliquer en commentaire
 		"""
-        
+		y_pred = np.array([self.predict(x, decision_tree) for x in X])
+		show_metrics(y, y_pred)
 	
-	# Vous pouvez rajouter d'autres méthodes et fonctions,
-	# il suffit juste de les commenter.
--- Code4/entrainer_tester.py
+++ Code4/entrainer_tester.py
@@ -2,7 +2,7 @@ import numpy as np
 import sys
 import load_datasets
 import DecisionTree # importer la classe de l'arbre de décision
-import NeuralNet# importer la classe du Knn
+import NeuralNet# importer la classe du DecisionTree
 #importer d'autres fichiers et classes si vous en avez développés
 
 from sklearn import neighbors # utilisation pour comaparer
@@ -17,6 +17,57 @@ En gros, vous allez :
 2- Charger les datasets
 3- Entraîner votre classifieur
 4- Le tester
+
+test = DecisionTree.DecisionTree(namesAtt=["Att1", "Att2"])
+train_labels = np.array([0, 0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 2])
+train = np.array([[1, 2, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1], [1, 1, 1, 1, 1, 2,2, 2, 1, 1, 2, 2], [0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0], [0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1]]).T
+#print(test.determineBestColumn(train, train_labels))
+decision_tree = test.train(train, train_labels)
+
+print(decision_tree)
+
+test.evaluate(train, train_labels, decision_tree)
 """
+train_ratio = 0.7
+
+# --> 1- Initialisation des classifieurs avec leurs paramètres
+classif_decisionTree_iris = DecisionTree.DecisionTree()
+classif_decisionTree_wine = DecisionTree.DecisionTree()
+classif_decisionTree_abalone = DecisionTree.DecisionTree()
+
+# --> 2- Chargement du dataset
+# 1) jeu iris
+iris = load_datasets.load_iris_dataset(train_ratio)
+# 2) jeu wine
+wine = load_datasets.load_wine_dataset(train_ratio)
+# 3) jeu abalone
+abalone = load_datasets.load_abalone_dataset(train_ratio)
+
+for dataset in ["iris"]:
+    train, train_labels, test, test_labels = eval(dataset)
+    ############################################################################
+    # DecisionTree
+    ############################################################################
+    classif_decisionTree = eval("classif_decisionTree_"+dataset)
+    print("\n######################################\nClassification DecisionTree / Dataset étudié = {}".format(dataset))
+
+    # --> Entraînement du classifieur
+    tps1 = perf_counter()
+    decision_tree = classif_decisionTree.train(train, train_labels)
+    print(decision_tree)
+
+    # --> Evaluation sur les données d'entraînement
+    print("\n######################################\nEvaluation sur les données d'entraînement")
+    classif_decisionTree.evaluate(train, train_labels, decision_tree)
+
+    # --> Evaluation sur les données de test
+    print("\n######################################\nEvaluation sur les données de test")
+    classif_decisionTree.evaluate(test, test_labels, decision_tree)
+    tps2 = perf_counter() # utilisé pour calculer les performances, avec seulement l'évaluation sur les données test et pas de print
+    print("\nTemps d'exécution :", tps2-tps1)
+
+
+
+
 
 
