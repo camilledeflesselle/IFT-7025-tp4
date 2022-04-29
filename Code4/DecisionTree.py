dab0a6835a35e972d12cdb3af2ee238373f164b2
deflessc <camille.deflesselle@edu.esiee.fr>
2022-04-29 18:59:52 -0400

    update

--- Code4/DecisionTree.py
+++ Code4/DecisionTree.py
@@ -15,74 +15,11 @@ import networkx as nx
 import pylab
 from scipy.stats import chi2, chi2_contingency
 import pandas as pd
+from drawTree import hierarchy_pos
 
 # le nom de votre classe
 # DecisionTree pour l'arbre de décision
 # NeuralNet pour le réseau de neurones
-def hierarchy_pos(G, root=None, width=1., vert_gap = 0.2, vert_loc = 0, xcenter = 0.5):
-
-    '''
-    From Joel's answer at https://stackoverflow.com/a/29597209/2966723.  
-    Licensed under Creative Commons Attribution-Share Alike 
-    
-    If the graph is a tree this will return the positions to plot this in a 
-    hierarchical layout.
-    
-    G: the graph (must be a tree)
-    
-    root: the root node of current branch 
-    - if the tree is directed and this is not given, 
-      the root will be found and used
-    - if the tree is directed and this is given, then 
-      the positions will be just for the descendants of this node.
-    - if the tree is undirected and not given, 
-      then a random choice will be used.
-    
-    width: horizontal space allocated for this branch - avoids overlap with other branches
-    
-    vert_gap: gap between levels of hierarchy
-    
-    vert_loc: vertical location of root
-    
-    xcenter: horizontal location of root
-    '''
-    if not nx.is_tree(G):
-        raise TypeError('cannot use hierarchy_pos on a graph that is not a tree')
-
-    if root is None:
-        if isinstance(G, nx.DiGraph):
-            root = next(iter(nx.topological_sort(G)))  #allows back compatibility with nx version 1.11
-        else:
-            root = random.choice(list(G.nodes))
-
-    def _hierarchy_pos(G, root, width=1, vert_gap = 10, vert_loc = 0, xcenter = 0, pos = None, parent = None):
-        '''
-        see hierarchy_pos docstring for most arguments
-
-        pos: a dict saying where all nodes go if they have been assigned
-        parent: parent of this branch. - only affects it if non-directed
-
-        '''
-    
-        if pos is None:
-            pos = {root:(xcenter,vert_loc)}
-        else:
-            pos[root] = (xcenter, vert_loc)
-        children = list(G.neighbors(root))
-        if not isinstance(G, nx.DiGraph) and parent is not None:
-            children.remove(parent)  
-        if len(children)!=0:
-            dx = width/len(children) *2
-            nextx = xcenter - width- dx/2
-            for child in children:
-                nextx += dx
-                pos = _hierarchy_pos(G,child, width = dx, vert_gap = vert_gap, 
-                                    vert_loc = vert_loc-vert_gap, xcenter=nextx,
-                                    pos=pos, parent = root)
-        return pos
-
-            
-    return _hierarchy_pos(G, root, width, vert_gap, vert_loc, xcenter)
 
 class DecisionTree: #nom de la class à changer
 
@@ -261,7 +198,7 @@ class DecisionTree: #nom de la class à changer
 		np.random.shuffle(indices)
 		train = train[indices]
 		train_labels = train_labels[indices]
-		for nb_instances in range(10, 100):
+		for nb_instances in range(1, 100):
 			train_used = train[0:nb_instances,:]
 			labels_train = train_labels[0:nb_instances]
 			test_used = train[nb_instances:100,:]
@@ -351,7 +288,6 @@ class DecisionTree: #nom de la class à changer
 		"""
 		isLeaf = True
 		parent = None
-
 		for key in list(decision_tree.keys()):
 			if isinstance(decision_tree[key], dict):
 				isLeaf = False
@@ -371,7 +307,6 @@ class DecisionTree: #nom de la class à changer
 			if len(question)<2 : 
 				sep = " == "
 				question = parent.split(' == ')
-				print(question)
 			attribute, value = question[0], question[1]
 			attribute = np.where([i == attribute for i in self.names])[0][0]
 			indexCorrect = eval("np.where(data[:, attribute]" + sep + value +")")[0]
@@ -399,12 +334,13 @@ class DecisionTree: #nom de la class à changer
 			if c > chi2.isf(q=alpha, df=dof):
 				self.satisfied_attributes.append(self.names[i])
 
-		pruned = True
-		while pruned and decision_tree != "pruned":
-			#keep pruning till leaf nodes can be pruned or till whole tree has been pruned
-			pruned = False
-			decision_tree = self.pruningLeaves(decision_tree, data, labels)
-			if decision_tree == 'pruned':
-				break
+		if isinstance(decision_tree, dict):
+			pruned = True
+			while pruned and decision_tree != "pruned":
+				#keep pruning till leaf nodes can be pruned or till whole tree has been pruned
+				pruned = False
+				decision_tree = self.pruningLeaves(decision_tree, data, labels)
+				if decision_tree == 'pruned':
+					break
 		#print(decision_tree)
 		return decision_tree
